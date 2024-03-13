d785906e6745492d88c553f96b8c688d01d11ef1
Harsh Wardhan <wardhan071@gmail.com>
2024-03-13 14:59:21 +0530

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+
+// express
+const express = require('express');
+const app = express();
+
+// database
+const connectDB = require('./db/connect');
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('E-commerce API');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
