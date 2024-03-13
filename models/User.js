a3cf2c4c5a2e230921f95b9a730a170539f4eae5
Harsh Wardhan <wardhan071@gmail.com>
2024-03-13 15:23:31 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlenght: 5,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+});
