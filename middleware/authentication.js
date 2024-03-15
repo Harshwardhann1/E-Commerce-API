5a4fa0d93f70abb18720ec046256ace8d23a6dd0
Harsh Wardhan <wardhan071@gmail.com>
2024-03-15 23:24:26 +0530

    first commit

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,17 +1,30 @@
-const CustomErr = require('../errors');
+const { CustomAPIError } = require('../errors');
 const { isTokenValid } = require('../utils');
 
 const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
 
   if (!token) {
-    console.log('error, no token present');
-  } else {
-    console.log('token present');
+    throw new CustomAPIError.UnauthenticatedError('Authentication Invalid');
   }
-  next();
+
+  try {
+    const { name, userId, role } = isTokenValid({ token });
+    req.user = { name, userId, role };
+    next();
+  } catch (error) {
+    throw new CustomAPIError.UnauthenticatedError('Authentication Invalid');
+  }
+};
+
+const authorizePermissions = (req, res, next) => {
+  if (req.user.role !== 'admin')
+    throw new CustomAPIError.UnauthorizedError(
+      'Unauthorized to access this route'
+    );
 };
 
 module.exports = {
   authenticateUser,
+  authorizePermissions,
 };
