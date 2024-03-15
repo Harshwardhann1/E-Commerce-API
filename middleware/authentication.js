518c2dd8d2938c40f692e03ff9558f3091228853
Harsh Wardhan <wardhan071@gmail.com>
2024-03-15 23:49:44 +0530

    first commit

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -17,11 +17,15 @@ const authenticateUser = async (req, res, next) => {
   }
 };
 
-const authorizePermissions = (req, res, next) => {
-  if (req.user.role !== 'admin')
-    throw new CustomAPIError.UnauthorizedError(
-      'Unauthorized to access this route'
-    );
+const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new CustomAPIError.UnauthorizedError(
+        'Unauthorized to access this route'
+      );
+    }
+    next();
+  };
 };
 
 module.exports = {
