d1d3429ffff524aa3c7a5d78ca2d774f6b82c532
Harsh Wardhan <wardhan071@gmail.com>
2024-03-17 18:20:39 +0530

    first commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,7 +15,9 @@ const getSingleProduct = async (req, res) => {
   const { id: productId } = req.params;
   const product = await Product.findOne({ _id: productId });
   if (!product) {
-    throw new CustomError.NotFoundError(`No product with id: ${productId}`);
+    throw new CustomError.NotFoundError(
+      `No product with id: ${productId}`
+    ).populate('reviews');
   }
   res.status(StatusCodes.OK).json({ product });
 };
@@ -64,7 +66,6 @@ const uploadImage = async (req, res) => {
   await productImage.mv(imagePath);
   res.status(StatusCodes.OK).json({ image: `/uploads/${productImage.name}` });
 };
-
 
 module.exports = {
   createProduct,
