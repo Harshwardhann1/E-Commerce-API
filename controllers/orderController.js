21cab7599551a84ee9e6f1e111c83368a14faca2
Harsh Wardhan <wardhan071@gmail.com>
2024-03-17 22:06:12 +0530

    first commit

---
+++ controllers/orderController.js
@@ -0,0 +1,25 @@
+
+const createOrder = async (req, res) => {
+  res.send('create order');
+};
+const getAllOrders = async (req, res) => {
+  res.send('get all orders');
+};
+const getSingleOrder = async (req, res) => {
+  res.send('get single order');
+};
+const getCurrentUserOrders = async (req, res) => {
+  res.send('get current user order');
+};
+const updateOrder = async (req, res) => {
+  res.send('update order');
+};
+
+module.exports = {
+  createOrder,
+  getAllOrders,
+  getSingleOrder,
+  getCurrentUserOrders,
+  updateOrder,
+};
+
